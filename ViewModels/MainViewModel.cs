684ecb5c13289e5a4269694c60b7b4ed389add2f
N4RNAC <n4rnac@outlook.com>
2026-09-20 16:11:04 +0800

    调整页面布局，完成QRCode生成

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -10,25 +10,36 @@ public partial class MainViewModel : ViewModelBase
 
     // Other properties and commands
     [ObservableProperty] public partial string IdInfo { get; set; } = idInfo();
-    [ObservableProperty] public partial string SourceUrl { get; set; } = "";
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(SongInfo))]
-    public partial string SongTitle { get; set; } = "";
+    [ObservableProperty] public partial string InfoBar { get; set; } = "Ready";
 
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(SongInfo))]
-    public partial string ArtistName { get; set; } = "";
+    [ObservableProperty] public partial string SourceUrl { get; set; }
+
+    [ObservableProperty] public partial string QrCodeData { get; set; }
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(SongInfo))] public partial string SongTitle { get; set; }
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(SongInfo))] public partial string ArtistName { get; set; }
+
+    [ObservableProperty] public partial string AlbumName { get; set; }
+
+    [ObservableProperty] public partial string SongStyle { get; set; }
 
     public string SongInfo => $"{SongTitle} - {ArtistName}";
 
-    [ObservableProperty] public partial string AlbumName { get; set; } = "";
 
-    [ObservableProperty] public partial string SongStyle { get; set; } = "";
-
-    [RelayCommand] private static void Generate()
+    [RelayCommand] private void Generate()
     {
-
+        var time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        if (string.IsNullOrEmpty(SourceUrl))
+        {
+            InfoBar = $"[{time}][ERROR] URL is not set or invalid.";
+        }
+        else
+        {
+            QrCodeData = SourceUrl;
+            InfoBar = $"[{time}][INFO] QrCode generate successful.";
+        }
     }
 
     [RelayCommand] private static void Save()
